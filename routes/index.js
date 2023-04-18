49ae4e85577a43f2159806898b60fa037e8b5de1
Lucashardouin <61150098+Lucashardouin@users.noreply.github.com>
2023-04-18 14:12:21 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var fetch =require('node-fetch');
+
+/* GET home page. */
+router.get('/movies', (req, res) => {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MY_API_KEY}`)
+  .then((res) => res.json())
+  .then((data) => {
+    res.json({movies: data.results});
+  });
+});
+
+module.exports = router;
